b0cc02e47d5247afe1fa9ba350c5bc217868eed8
swapnali213 <99112506+swapnali213@users.noreply.github.com>
2022-10-20 11:51:51 +0530

    second commit

---
+++ Labassignent1/src/assign/transposemetrix.java
@@ -0,0 +1,25 @@
+package assign;
+
+public class transposemetrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][]a ={{1,2},{4,5},{8,9}};
+		int[][]transpose= new int[2][2];
+		
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				transpose[i][j]=a[j][i];  
+			}
+			
+		}
+		System.out.println(" Matrix After Transpose:");  
+		for(int i=0;i<2;i++){    
+		for(int j=0;j<2;j++){  
+			System.out.print(transpose[i][j]+" ");    
+	}
+		System.out.println();   
+
+}}}
