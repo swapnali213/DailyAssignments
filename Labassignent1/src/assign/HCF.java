eb7c2d0dde617d53c699c079f5c7eb8683155b17
swapnali213 <99112506+swapnali213@users.noreply.github.com>
2022-10-20 12:25:28 +0530

    commit

---
+++ Labassignent1/src/assign/HCF.java
@@ -0,0 +1,21 @@
+package assign;
+
+public class HCF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		    int num1 = 36, num2 = 60, hcf=0;
+
+		    for (int i = 1; i <= num1 || i <= num2; i++)
+		      {
+		     if (num1 % i == 0 && num2 % i == 0)
+		        hcf = i;
+		      }
+
+		    System.out.println("The HCF: "+ hcf);
+		  }
+		
+
+	}
+
